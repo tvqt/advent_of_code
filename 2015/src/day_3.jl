43a0e3cee4a2e4c8b4814c8b366779df0ed29e26
Mark Hill <markh177@gmail.com>
2023-03-20 20:36:28 +1400

    added comments to day 3

--- 2015/src/day_3.jl
+++ 2015/src/day_3.jl
@@ -3,7 +3,7 @@
 
 input = split(readline("2015/data/day_3.txt"),"")
 
-function move(position, direction)
+function move(position, direction) # move in a direction
     if direction == "^"
         return (position[1], position[2]+1)
     elseif direction == "v"
@@ -15,39 +15,31 @@ function move(position, direction)
     end
 end
 
-function part_1(input)
+function part_1(input) 
     position = (0,0)
-    visited = []
-    push!(visited, position)
-    for i in input
-        position = move(position, i)
-        if !in(position, visited)
-            push!(visited, position)
-        end
+    visited = Set([(0,0)])
+    for i in input # for each directive
+        position = move(position, i) # move in that direction
+        push!(visited, position) # add the new position to the set
     end
-    return length(visited)
+    return length(visited) # return the length of the set
 end
 @show part_1(input)
 
 function part_2(input)
-    position = (0,0)
-    position2 = (0,0)
-    visited = []
-    push!(visited, position)
-    for i in eachindex(input)
-        if i % 2 == 0
-            position = move(position, input[i])
-            if !in(position, visited)
-                push!(visited, position)
-            end
+    position = (0,0) # santa's position
+    position2 = (0,0) # robo-santa's position
+    visited = Set([(0,0)])
+    for i in eachindex(input) # for each directive
+        if i % 2 == 0 # if even
+            position = move(position, input[i]) # move santa
+            push!(visited, position) # add the new position to the set
         else
-            position2 = move(position2, input[i])
-            if !in(position2, visited)
-                push!(visited, position2)
-            end
+            position2 = move(position2, input[i]) # move robo-santa
+            push!(visited, position2) # add the new position to the set
         end
     end
-    return length(visited)
+    return length(visited) # return the length of the set
 
 end
-@info part_2(input)
+@show part_2(input)

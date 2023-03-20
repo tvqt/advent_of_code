6fbeee323ad10196b8278cb3f8287d86aa6c210b
Mark Hill <markh177@gmail.com>
2023-03-20 20:23:44 +1400

    Added comments

--- 2015/src/day_2.jl
+++ 2015/src/day_2.jl
@@ -7,8 +7,10 @@ function solve(part::Int=1, input=input)
     total = 0
     for (l, w, h) in input
         if part == 1
+            # calculate the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l.
             total += 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
         else
+            # The elves also need a little extra paper for each present: the area of the smallest side.
             total += 2*min(l+w, w+h, h+l) + l*w*h
         end
     end

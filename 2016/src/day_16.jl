8d5b0ba404ce9bd64c641c59caedd46d66a9e20b
Mark Hill <markh177@gmail.com>
2023-04-16 01:21:46 +1400

    16, 2016

--- 2016/src/day_16.jl
+++ 2016/src/day_16.jl
@@ -1,13 +1,14 @@
+# Day 16: Dragon Checksum
 # https://adventofcode.com/2016/day/16
 
-input = readlines("2016/data/day_16.txt")
+input = "00111101111101000"
 
-function part_1(input)
-    nothing
-end
-@info part_1(input)
+length1 = 272
+length2 = 35651584
 
-function part_2(input)
-    nothing
-end
-@info part_2(input)
+dragon_curve(input, target_length) = while length(input) < target_length; input = dragon_creator(input); end; return checksum(input[1:target_length])
+dragon_creator(input) = input * "0" * replace(reverse(input), '0' => '1', '1' => '0') 
+checksum(input) = while length(input) % 2 == 0; input = replace(input, r"00|11" => "1", r"01|10" => "0"); end; return input
+
+@show dragon_curve(input, length1)
+@show dragon_curve(input, length2)
